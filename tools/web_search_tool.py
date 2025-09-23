f7aed0d23206e90486cb5a09abe19313a7c71149
rohitjg13 <jgrohit.exe@gmail.com>
2025-09-23 14:47:03 +0530

    serper implimentation

--- tools/web_search_tool.py
+++ tools/web_search_tool.py
@@ -2,31 +2,34 @@ from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.tools import tool
-from googlesearch import search
+from googlesearch.googlesearch import GoogleSearch
+from langchain_community.utilities import GoogleSerperAPIWrapper
 
 load_dotenv()
 
 llm = init_chat_model("llama-3.3-70b-versatile", model_provider="groq")
 
-@tool
-def search_google(query: str) -> str:
-    "Searches the web using google search and returns the top result."
-    return search(query)
-
-tools = [search_google]
-llm = llm.bind_tools(tools)
+search = GoogleSerperAPIWrapper()
+tools = [
+    Tool(
+        name="Intermediate Answer",
+        func=search.run,
+        description="useful for when you need to ask with search"
+    )
+]
 
 def main():
-    messages = [
-        SystemMessage("You are an AI assistant that can use tools to help the user search the web and get realtime data."),
-        HumanMessage("What is the current weather in delhi?")
-    ]
-    response = llm.invoke(messages)
-    print(response.content)
-    print(response.tool_calls)
+    query = input("Please enter your search query: ")
+    self_ask_with_search = initialize_agent(tools, llm, agent=AgentType.SELF_ASK_WITH_SEARCH, verbose=True)
+    self_ask_with_search.run(quert)
 
-    for t in response.tool_calls:
-        print(t)
-        if t['name'] == 'search_google':
-            tool_response = search_google(t['arguments']['query'])
-            print(f"Tool {t['name']} response: {tool_response}")
+
+
+    # for t in response.tool_calls:
+    #     print(t)
+    #     if t['name'] == 'search_google':
+    #         tool_response = search_google(t['arguments']['query'])
+    #         print(f"Tool {t['name']} response: {tool_response}")
+
+if __name__ == "__main__":
+    main()
