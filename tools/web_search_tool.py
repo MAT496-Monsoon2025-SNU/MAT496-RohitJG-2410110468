8572fbb357ffbad39ae95991742e54959bdd3abc
rohitjg13 <jgrohit.exe@gmail.com>
2025-09-23 14:54:37 +0530

    serper fix with handle error retry

--- tools/web_search_tool.py
+++ tools/web_search_tool.py
@@ -4,7 +4,8 @@ from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.tools import tool
 from googlesearch.googlesearch import GoogleSearch
 from langchain_community.utilities import GoogleSerperAPIWrapper
-
+from langchain.agents import initialize_agent, Tool
+from langchain.agents import AgentType
 load_dotenv()
 
 llm = init_chat_model("llama-3.3-70b-versatile", model_provider="groq")
@@ -20,8 +21,8 @@ tools = [
 
 def main():
     query = input("Please enter your search query: ")
-    self_ask_with_search = initialize_agent(tools, llm, agent=AgentType.SELF_ASK_WITH_SEARCH, verbose=True)
-    self_ask_with_search.run(quert)
+    self_ask_with_search = initialize_agent(tools, llm, agent=AgentType.SELF_ASK_WITH_SEARCH, verbose=True, handle_parsing_errors=True)
+    self_ask_with_search.run(query)
 
 
 
